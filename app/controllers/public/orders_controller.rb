f95818169a96f15f25431143a278399c1c953fd9
EC2 Default User <ec2-user@ip-172-31-36-51.ap-northeast-1.compute.internal>
2023-06-04 07:21:11 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -15,8 +15,20 @@ class Public::OrdersController < ApplicationController
     end
     
     def create
-        @order = Order.new(order_params)
+        @order = Order.new(orders_params)
         @order.save
+        current_customer.cart_items.each do |cart|
+                  order_item = OrderDetail.new
+                  order_item.item_id = cart.item_id
+                  order_item.order_id = @order.id
+                  order_item.amount = cart.amount
+                  order_item.price = cart.item.price
+                  order_item.save
+        end
+        current_customer.cart_items.destroy_all
+        
+        redirect_to orders_complete_path
+        
     end
     
     def index
@@ -25,7 +37,7 @@ class Public::OrdersController < ApplicationController
 
     private
      def orders_params
-     params.require(:order).permit(:postage, :payment_methpd, :postal_code, :address, :last_name, :first_name, :total_payment, )
+     params.require(:order).permit(:postage, :payment_way, :postal_code, :address, :last_name, :first_name, :total_payment, )
      
      end
 end
